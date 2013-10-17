26e94dcd2cae819a595b09e6fc33f20f3a320bb2
Clare Liu <clareliu727@gmail.com>
2013-10-17 02:08:00 -0600

    Some major changes, basic outline of ATM program completed

--- Atm.java
+++ Atm.java
@@ -1,35 +1,94 @@
 import java.util.Scanner;
 
 public class Atm {
-	private Scanner lala = new Scanner();
-	private int[] array = new int[3];
-	private Account bob = new Account("bob", 1234, 100);
-	private Account alice = new Account("alice", 3333, 250);
-	private Account jt= new Account("JT", 0001, 999999999);
+
+	// State
+	private Scanner input = new Scanner(System.in);
+	private Account[] array = new Account[3];
+        private Account loginAccount = null;
+        private boolean loginFlag = false;
+
+        // Constructor
+        public Atm() {
+                this.array[0] = new Account("Bob", "1234", 100);
+	        this.array[1] = new Account("Alice", "3333", 250);
+	        this.array[2] = new Account("JT", "0001", 999999999);
+        }
+
+        public boolean getLoginFlag() {
+                return loginFlag;
+        }
 
 	public void login() {
 		String thisName = "";
-		int thisPin = 0;
-		this.array[0] = this.bob;
-		this.array[1] = this.alice;
-		this.array[2] = this.jt;
-		System.out.print("Name:");
-		thisName = lala.next(java.lang.String);
-		System.out.print("Pin:");
-		thisPin = lala.nextInt();
+		String thisPin = "";
+		
+                do {
+		     System.out.print("Name: ");
+		     thisName = input.nextLine();
+                } while (!checkName(thisName));
 
-		if () {
-				
-			}	
+                do {
+		     System.out.print("PIN: ");
+		     thisPin = input.nextLine();
+                } while (!checkPin(thisPin));
+	
 	}
+
+        //checks if name exists
+        public boolean checkName(String thisName) {
+                for (int i = 0; i < array.length; i++) {
+                     Account account = array[i];
+                     if (account.getName().equals(thisName)) {
+                        loginAccount = account;
+                        return true;
+                     }
+                 }
+                 System.out.println("Account " + thisName + " does not exist.");
+                 return false;
+        }
+
+        //checks if pin corresponds to account name
+        public boolean checkPin(String thisPin) {
+                if (loginAccount != null) {
+                     if (loginAccount.getPin().equals(thisPin)) {
+                         loginFlag = true;
+                         return true;
+                     }
+                } 
+              System.out.println("PIN is incorrect.");
+              return false;  
+        }
+
+
 	public void checkBalance() {
-		System.out.println(current.getBalance());
+                if (loginFlag) {
+		     System.out.println("Balance is: " + loginAccount.getBalance());
+                }
 	}
-	public void withdrawFunds() {
-		setBalance(balance - lala.nextInt());
+
+	public void withdrawFunds(double amount) {
+		if (amount > 0.0 && loginFlag) {
+                      loginAccount.setBalance(loginAccount.getBalance() - amount);
+                      System.out.println("New balance is: " + loginAccount.getBalance());
+                }
 	}
-	public void changeName() {
-		setName(lala.nextInt)
+	public void changeName(String newName) {
+		if (loginFlag) {
+                     loginAccount.setName(newName);
+                     System.out.println("New account name is: " + loginAccount.getName());
+                }
+	}
+        public void changePin(String newPin){
+		if (loginFlag) {
+                     loginAccount.setPin(newPin);
+                     System.out.println("New PIN is: " + loginAccount.getPin());
+                }
+	}
+	public void logout() {
+                loginFlag = false;
+                loginAccount = null;
+                login();
 	}
 
 
