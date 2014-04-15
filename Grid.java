7dccdb80077481dc5f68eab565a7d4df7a27e86d
Treece <Pashweetie@gmail.com>
2014-04-15 08:52:16 -0600

    I believe I can fly

--- Grid.java
+++ Grid.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 public class Grid {
-	private ArrayList<ArrayList<Integer>> grid = new ArrayList<ArrayList<Integer>>();
+	private int[][] arrayYo = new int[8][8];
 	public Grid(){
-
+		for (int a = 0;a<9: a++) {
+			for (int b = 0; b<9; b++) {
+				arrayYo[a][b] = 0;
+			}
+		}
 	}
-	
+
 }
---
+++ Point.java
@@ -0,0 +1,11 @@
+public class Point {
+	private int [] a = new int[2];
+	public Point(int i, int g){
+		a[0] = i;
+		a[1] = g;
+	}
+	public int[] get(){
+		return a+b;
+	}
+		
+}
