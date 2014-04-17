157f0cf08bb16810f36f1a4d5bff0ab8edd06d30
Treece <Pashweetie@gmail.com>
2014-04-17 09:32:33 -0600

    Started up Square class in process of messing with grid class

--- Knight.java
+++ Knight.java
@@ -5,11 +5,14 @@ public class Knight extends Piece{
 		super("Knight", k);
 	}
 	public void move(Point k){
-		if((super.k.getY()-j)/2 == super.k.getX()-i && k.getY() !> 8 k.getX() !> 8){
+		if((super.k.getY()-j)/2 == super.k.getX()-i && k.getY() !> 8 k.getX() !> 8 && grid.get(k) == null){
 			//moves piece to loc
 		}
-		if(super.k.getY()-j == (super.k.getX()-i)/2 && k.getY() !> 8 k.getX() !> 8){
+		else if(super.k.getY()-j == (super.k.getX()-i)/2 && k.getY() !> 8 k.getX() !> 8 && grid.get(k) == null){
 			//move piece to loc
+		}
+		else{
+
 		}
 	}
 }
