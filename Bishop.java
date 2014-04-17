973cf42c76579b9d203402e466e96b909287eb99
Treece <Pashweetie@gmail.com>
2014-04-17 08:48:02 -0600

    Nearly finished bishop

--- Bishop.java
+++ Bishop.java
@@ -1,12 +1,12 @@
+import  java.lang.Math.*;
 public class Bishop extends Piece {
 	private int x = 0;
 	private int y = 0
-	public Bishop(int i, int j){
-		x=i;
-		y=j;
+	public Bishop(Point k){
+		super("Bishop", k);
 	}
 	public void move(Point k){
-		if(k.getY-j == k.getX-i && k.getY !> 8 k.getX !> 8){
+		if(Math.abs(super.k.getY()-y) == Math.abs(super.k.getX()-x) && super.k.getY() !> 8 super.k.getX !> 8){
 
 		}
 	}
