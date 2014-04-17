c5362e48ec20f3d4def65efd2b85938282dad4ac
apcs <apcs@mvhs-imac05u528.dcsdk12.org>
2014-04-17 08:39:21 -0600

    Pawn??

--- Pawn.java
+++ Pawn.java
@@ -1,7 +1,15 @@
 public class Pawn extends Piece{
 
-	public Pawn() {
-		
+	public Pawn(Point p) {
+		super("Pawn", p);
+	}
+
+	public void move(Point k) {
+		if (k.getX() == super.location.getX() && k.getY() < 8 && k.getY > 0) {
+			super.location == k;
+		} else if (k.getY() == super.location.getY() && k.getX() < 8 && k.getX > 0) {
+			super.location == k;
+		}
 	}
 
 
