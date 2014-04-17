50eec28b76856c1caf2fd16e14eee76e46d360b3
Treece <Pashweetie@gmail.com>
2014-04-17 08:31:08 -0600

    Updated bishop

--- Knight.java
+++ Knight.java
@@ -1,3 +1,13 @@
 public class Knight extends Piece{
-	
+	private int x = 0;
+	private int y = 0
+	public Bishop(int i, int j){
+		x=i;
+		y=j;
+	}
+	public void move(Point k){
+		if(k.getY-j == k.getX-i && k.getY !> 8 k.getX !> 8){
+
+		}
+	}
 }
