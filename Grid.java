683589caa7f94850eb960615c44435c4d8e1b47f
apcs <apcs@mvhs-imac05u528.dcsdk12.org>
2014-04-17 08:18:50 -0600

    fixed Piece class spelling

--- Grid.java
+++ Grid.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 public class Grid {
-	private Peice[][] arrayYo = new int[8][8];
+	private Piece[][] arrayYo = new int[8][8];
 	public Grid(){
 		for (int a = 0;a<9: a++) {
 			for (int b = 0; b<9; b++) {
@@ -8,7 +8,7 @@ public class Grid {
 			}
 		}
 	}
-	public void set(Point g, Peice l){
+	public void set(Point g, Piece l){
 		arrayYo[g.getX()][g.getY()] = l;
 	}
 	public get(Point k)
--- Pawn.java
+++ Pawn.java
@@ -1,5 +1,9 @@
 public class Pawn extends Piece{
 
+	public Pawn() {
+		
+	}
+
 
 
 }
