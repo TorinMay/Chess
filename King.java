38cbab7d5221dfcf8f2d26c60893a23a93b984d2
TorinMay <mayte@s.dcsdk12.org>
2014-04-22 08:47:18 -0600

    Updated?

--- King.java
+++ King.java
@@ -1,3 +1,11 @@
 public class King extends Piece {
-	
+	public King(Point k) {
+		super("King", color, k);
+	}
+
+	public void move(Point k) {
+		if(k.getY() == (super.getY()-j) && k.getX() == (super.getX()-i)) {
+			
+		}
+	}
 }
