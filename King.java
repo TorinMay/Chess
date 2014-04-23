fc54c044f17156510424616d895c22cad7a469f9
TorinMay <mayte@s.dcsdk12.org>
2014-04-22 19:37:35 -0600

    Finished Move on king and queen

--- King.java
+++ King.java
@@ -1,11 +1,19 @@
 public class King extends Piece {
-	public King(Point k) {
+	public King(Point k, String color) {
 		super("King", color, k);
 	}
 
 	public void move(Point k) {
-		if(k.getY() == (super.getY()-j) && k.getX() == (super.getX()-i)) {
-			
+		if (isMoveLegal(k)) {
+			grid.set(k, this);
 		}
+	}
+	public boolean isMoveLegal(Point k) {
+		if((k.getY() >= (super.getY()-1) && (k.getY() <= (super.getY()+1)) {
+			if((k.getX() >= (super.getX()-1) && (k.getX() <= (super.getX()+1)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- Queen.java
+++ Queen.java
@@ -1,14 +1,29 @@
 public class Queen extends Piece {
-	public Queen(Point k) {
+	public Queen(Point k, String color) {
 		super("Queen", color, k);
 	}
 
-	public Location move(Point k) {
-		Location locY = new Location();
-		Location locX = new Location();
-		if(k.getY() == (super.getY()) && k.getX() == (super.getX())) {
-			locY = (super.getY()-1);
-			locX = (super.getX()-1);
+	public void move(Point k) {
+		if (isMoveLegal(k)) {
+			grid.set(k,this);
 		}
+	}
+	public boolean isMoveLegal(Point k) {
+		int deltaX = super.getX() - k.getX();
+		int deltaY = super.getY() - k.getY();
+		if(k.getY() == super.getY()) {
+			return true;
+			
+		}else if(k.getX() == super.getX()) {
+			return true;
+
+		}else if(deltaX == deltaY) {
+			return true;
+
+		}else if(deltaX == -deltaY) {
+			return true;
+
+		}
+		return false;
 	}
 }
