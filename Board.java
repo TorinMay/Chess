499b2636bc9f1848816327bee85d1233dba6ad78
TorinMay <mayte@s.dcsdk12.org>
2014-05-16 08:50:30 -0600

    Yustin

--- Board.java
+++ Board.java
@@ -61,7 +61,7 @@ public class Board extends JPanel{
 
 	public static void readImgs() {
 		try {
-    		kingImage = ImageIO.read(new File("/Users/apcs/projects/Chess/img/king_white.png"));
+    		kingImage = ImageIO.read(new File("/Users/apcs/projects/Chess/img/king_black.png"));
 		} catch (IOException e) {
 			System.out.println("Error reading file " + e.getMessage());
 		}
