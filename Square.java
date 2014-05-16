6e831307d9ef69839b9733f208fd97de3cd8a18a
Treece <Pashweetie@gmail.com>
2014-05-16 08:30:56 -0600

    Finally FAB

--- Square.java
+++ Square.java
@@ -6,10 +6,10 @@ public class Square{
 	private Piece z;
 	public Square(String a, Point b, Piece k){
 		if(a.equals("Black")){
-			c = new Color(92,64,51);
+			c = new Color(54,29,0);
 		}
 		else{
-			c = new Color(255,190,228);
+			c = new Color(255,240,195);
 		}
 		this.b = b;
 		z=k;
